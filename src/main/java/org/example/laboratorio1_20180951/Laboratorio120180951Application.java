956d524b5f2c6fd5f46d16532569173cfd145a85
SergioABS-GTICS <a20213170@pucp.edu.pe>
2024-04-03 21:55:28 -0500

    first commit

---
+++ src/main/java/org/example/laboratorio1_20180951/Laboratorio120180951Application.java
@@ -0,0 +1,13 @@
+package org.example.laboratorio1_20180951;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio120180951Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboratorio120180951Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/laboratorio1_20180951/Laboratorio120180951ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.laboratorio1_20180951;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio120180951ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
